3cf0e371857b13291aa52d1896e7c22e85d4582e
freddy2711 <freddy2711@gmail.com>
2023-01-05 16:27:39 -0500

    update

--- components/UI/molecules/viewList/ViewList.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/react'
-import ViewList from './ViewList'
-
-storiesOf('Molecules/viewList', module).add('view-List', () => (
-  <ViewList
-    texLabel={'demo'}
-    optionSelect={[]}
-  />
-))
--- components/UI/molecules/viewTexarea/ViewTexarea.stories.tsx
+++
@@ -1,6 +0,0 @@
-import { storiesOf } from '@storybook/react'
-import ViewTexarea from './ViewTexarea'
-
-storiesOf('Molecules/viewTexarea', module).add('view-Texarea', () => (
-  <ViewTexarea texLabel={'response'} />
-))
